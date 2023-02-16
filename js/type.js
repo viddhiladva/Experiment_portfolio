0b05c689321b8f3243934e73761f85e265f71d43
Viddhi Ladva <109016916+viddhiladva@users.noreply.github.com>
2023-02-16 10:44:17 +0530

    Add files via upload

---
+++ js/type.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".profession",{
+  strings:[" ","Web Designer .","Web Developer under Learning .","Passionate in Full Stack Web Development ."],
+  typeSpeed:150,
+  BackSpeed:10,
+  loop:true
+})
